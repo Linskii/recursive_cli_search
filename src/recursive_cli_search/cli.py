864dd4edf5fc8b2ad550cfc63b442b7379a84e99
Paul Link <paullink65@gmail.com>
2025-07-29 10:36:15 +0200

    Added subprocesses to gain access to helper functions

--- src/recursive_cli_search/cli.py
+++ src/recursive_cli_search/cli.py
@@ -1,10 +1,49 @@
 import click
+import subprocess
+from typing import List
+from anytree import Node
+from collections import deque
 
 
 @click.command()
 @click.argument("searchword")
 @click.argument("commandname")
 def cli(searchword, commandname):
+    root = Node(commandname)
+
+    recursive_help(root)
+
+
+def recursive_help(command_node: Node) -> str:
+    try:
+        command_chain = deque([command_node.name])
+        while command_node.parent:
+            command_node = command_node.parent
+
+            command_chain.appendleft(command_node.name)
+
+        command = list(command_chain) + ["--help"]
+        print(command)
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        if not result.stdout.strip():
+            print("Command ran but returned no output.")
+
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with exit code {e.returncode}")
+        print(e.stderr)
+        return ""
+    except FileNotFoundError:
+        print("Command not found.")
+        return ""
+
+
+def parse_output(output: str) -> List[str]:
     pass
 
 
